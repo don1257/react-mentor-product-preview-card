3807ab31755bfec5e88c84b5a7623b3bef421028
don1257 <jp-hok>
2023-07-27 18:18:13 -0400

    Adding mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from "./Components/Card/Card";
+import mobileDesign from "./assets/mobile-design.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        <Card></Card>
+          <br/>
+        <img
+          src={mobileDesign}
+        />
+      </div>
+
   );
 }
 
---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,39 @@
+import './Card.css'
+import productMobile from '../../assets/image-product-mobile.jpg'
+import iconCart from '../../assets/icon-cart.svg'
+
+const Card = () => {
+
+    return (
+        <>
+            <div className='card-container'>
+
+                <div className='card'>
+                    <img
+                        src={productMobile}
+                    />
+                    <div className='card-content'>
+                        <div className='card-text-container'>
+                            <p className='card-category-title'> P E R F U M E</p>
+                            <p className='card-title'>Garbrielle Essence Eau De Parfum</p>
+                            <p className='card-description'>A floral, solar, and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                            <div className='card-container-price'>
+                                <p className='card-price'>$149.99</p>
+                                <p className='card-reduce-price'>$169.99</p>
+                            </div>
+                            <div className='card-button'>
+                                <img className='card-cart-svg' src={iconCart} alt="Cart" />
+                                Add to Cart
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
